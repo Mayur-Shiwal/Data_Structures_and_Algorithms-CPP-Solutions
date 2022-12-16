bb2a3343958a36829044b02b25407472a70de652
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2022-12-16 19:26:13 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Reorder List/reorder-list.cpp
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Following is the Linked list node structure */
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{ 
+public:
+    Node* reverseList(Node* head){
+        Node* prev = NULL;
+        Node* curr = head;
+        Node* nxt = NULL;
+        while(curr != NULL){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+    void reorderList(Node* head) {
+        // Your code here
+        //find mid using slow ans fast pointer;
+        Node* slow = head;
+        Node* fast = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        //deviding both list and reversing the second list
+        Node* second = reverseList(slow->next);
+        slow->next = NULL;
+        Node* first = head;
+        //merging both the list
+        while(second){
+            Node* temp1 = first->next;
+            first->next = second;
+            second = second->next;
+            first->next->next = temp1;
+            first = temp1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+/* Function to create a new Node with given data */
+struct Node* newNode(int data) {
+    struct Node* new_Node = new Node(data);
+    new_Node->data = data;
+    new_Node->next = NULL;
+    return new_Node;
+}
+
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int t, n, m, i, x;
+
+    cin >> t;
+    while (t--) {
+        struct Node* temp, *head;
+        cin >> n;
+        cin >> x;
+        head = new Node(x);
+        temp = head;
+        for (i = 0; i < n - 1; i++) {
+            cin >> x;
+            temp->next = new Node(x);
+            temp = temp->next;
+        }
+        
+        Solution ob;
+        
+        ob.reorderList(head);
+        printList(head);
+        freeList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
