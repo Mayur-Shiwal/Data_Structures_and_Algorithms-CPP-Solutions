7c43029b397f1ff981f77759c016f85f6b5fb16d
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2022-12-16 19:46:06 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Account Merge/account-merge.cpp
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class DisjoinSet{
+    vector<int> rank, parent, size;
+public:
+    DisjoinSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUPar(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v){
+        int ultimateParent_u = findUPar(u);
+        int ultimateParent_v = findUPar(v);
+        if(ultimateParent_u == ultimateParent_v) return;
+        if(rank[ultimateParent_u] < rank[ultimateParent_v]){
+            parent[ultimateParent_u] = ultimateParent_v;
+        }
+        else if(rank[ultimateParent_v] < rank[ultimateParent_u]){
+            parent[ultimateParent_v] = ultimateParent_u;
+        }
+        else{
+            parent[ultimateParent_v] = ultimateParent_u;
+            rank[ultimateParent_u]++;
+        }
+    }
+    void unionBySize(int u, int v){
+        int ultimateParent_u = findUPar(u);
+        int ultimateParent_v = findUPar(v);
+        if(ultimateParent_u == ultimateParent_v) return;
+        if(size[ultimateParent_u] < size[ultimateParent_v]){
+            parent[ultimateParent_u] = ultimateParent_v;
+            size[ultimateParent_v] += size[ultimateParent_u];
+        }
+        else{
+            parent[ultimateParent_v] = ultimateParent_u;
+            size[ultimateParent_u] += size[ultimateParent_v];
+        }
+    }
+
+};
+class Solution{
+  public:
+    vector<vector<string>> accountsMerge(vector<vector<string>> &accounts) {
+        // code here
+        int n = accounts.size();
+        DisjoinSet ds(n);
+        unordered_map<string, int> mapMailNode;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string mail = accounts[i][j];
+                if(mapMailNode.find(mail) == mapMailNode.end()){
+                    mapMailNode[mail] = i;
+                }
+                else{
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+        vector<string> mergedMail[n];
+        for(auto it: mapMailNode){
+            string mail = it.first;
+            int node = ds.findUPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+        vector<vector<string>> ans;
+        for(int i=0;i<n;i++){
+            if(mergedMail[i].size() == 0) continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it: mergedMail[i]) temp.push_back(it);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    vector<vector<string>> accounts;
+
+    for (int i = 0; i < n; i++)
+    {
+      vector<string> temp;
+      int x;
+      cin >> x;
+
+      for (int j = 0; j < x; j++)
+      {
+        string s1;
+        cin >> s1;
+        temp.push_back(s1);
+      }
+      accounts.push_back(temp);
+    }
+
+    Solution obj;
+    vector<vector<string>> res = obj.accountsMerge(accounts);
+    sort(res.begin(), res.end());
+    cout << "[";
+    for (int i = 0; i < res.size(); ++i)
+    {
+      cout << "[";
+      for (int j = 0; j < res[i].size(); j++)
+      {
+        if (j != res[i].size() - 1)
+          cout << res[i][j] << ","
+               << " ";
+        else
+          cout << res[i][j];
+      }
+      if (i != res.size() - 1)
+        cout << "], " << endl;
+      else
+        cout << "]";
+    }
+    cout << "]"
+         << endl;
+  }
+}
+// } Driver Code Ends
