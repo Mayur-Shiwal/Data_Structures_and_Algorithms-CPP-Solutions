7639ed9f2d8206e89f44660e8037cb46177cea98
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2022-12-16 19:28:45 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// UNASSIGNED is used for empty cells in sudoku grid 
+#define UNASSIGNED 0  
+
+// N is used for the size of Sudoku grid.  
+// Size will be NxN  
+#define N 9  
+
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    int isValid(int grid[N][N], int c, int row, int col){
+        for(int i=0;i<9;i++){
+            if(grid[i][col] == c) return false;
+            if(grid[row][i] == c) return false;
+            if(grid[3 * (row/3) + i/3][3 * (col/3) + i%3] == c) return false;
+        }
+        return true;
+    }
+    //Function to find a solved Sudoku. 
+    bool SolveSudoku(int grid[N][N])  
+    { 
+        // Your code here
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(grid[i][j] == 0){
+                    for(int c=1;c<=9;c++){
+                        if(isValid(grid, c, i, j)){
+                            grid[i][j] = c;
+                            if(SolveSudoku(grid) == true) return true;
+                            else grid[i][j] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    //Function to print grids of the Sudoku.
+    void printGrid (int grid[N][N]) 
+    {
+        // Your code here
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cout<<grid[i][j]<<" ";
+            }
+            
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int grid[N][N];
+		
+		for(int i=0;i<9;i++)
+		    for(int j=0;j<9;j++)
+		        cin>>grid[i][j];
+		        
+		Solution ob;
+		
+		if (ob.SolveSudoku(grid) == true)  
+            ob.printGrid(grid);  
+        else
+            cout << "No solution exists";  
+        
+        cout<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
