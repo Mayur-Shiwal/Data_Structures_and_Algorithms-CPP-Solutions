b8ece21f2f235a5f19586da5be7682d4afe8c86d
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-01-14 16:48:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Largest odd number in string/largest-odd-number-in-string.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string maxOdd(string num) {
+        // your code here
+        int i;
+        for(i=num.size()-1;i>=0;i--){
+            if((num[i] - 48) % 2 != 0) break;
+        }
+        return num.substr(0, i+1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+
+        cout << ob.maxOdd(s) << endl;
+    }
+}
+
+// } Driver Code Ends
