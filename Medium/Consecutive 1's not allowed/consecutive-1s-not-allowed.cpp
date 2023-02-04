bbee2fba78d0f3cc6bab1c786645f68adcce7120
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-02-04 19:59:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	ll countStrings(int n) {
+	    // code here
+	    int zeroEnd = 1;
+	    int oneEnd = 1;
+	    int sum = zeroEnd + oneEnd;
+	    if(n == 1) return sum;
+	    int i = 2;
+	    while(i <= n){
+	        oneEnd = zeroEnd % 1000000007;
+	        zeroEnd = sum % 1000000007;
+	        sum = (oneEnd + zeroEnd) % 1000000007;
+	        i++;
+	    }
+	    return sum;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
