cb6cfc680acf4906fc4530caf3ae512e1326186c
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-03-31 16:31:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Even Swap/even-swap.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+        vector <int> lexicographicallyLargest(vector <int> &a,int n)
+        {
+            // Code here
+            vector<int> nums = a;
+            int i = 0;
+            while(i < n){
+                int j = i+1;
+                while(j < n && ((nums[j] + nums[j-1])%2) == 0) j++;
+                sort(nums.begin()+i, nums.begin()+j, greater<int>());
+                i = j;
+            }
+            return nums;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector <int> a(n);
+        for(auto &j:a)
+            cin>>j;
+        Solution s;
+        vector <int> b=s.lexicographicallyLargest(a,n);
+        for(auto i:b)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
