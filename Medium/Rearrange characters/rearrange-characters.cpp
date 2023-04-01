132188cb010166287373fb3bc2e3c55ef71fe222
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-04-01 12:53:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rearrange characters/rearrange-characters.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+const int MAX_CHAR = 26;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    string rearrangeString(string str)
+    {
+        //code here
+        map<char, int> mp;
+        for(auto it: str) mp[it]++;
+        priority_queue<pair<int, char>> pq;
+        for(auto it: mp) pq.push({it.second, it.first});
+        pair<int, char> p = {-1, '#'};
+        string ans = "";
+        while(!pq.empty()){
+            auto frontEle = pq.top();
+            pq.pop();
+            ans += frontEle.second;
+            frontEle.first--;
+            if(p.first>0) pq.push(p);
+            p = frontEle;
+        }
+        return (str.length() == ans.length()) ? ans : "-1";
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin>>str;
+	    Solution ob;
+	    string str1=ob.rearrangeString(str);
+	    int flag=1;
+	    int c[26] = {0};
+	    for(int i=0; i<str.length(); i++)
+	        c[str[i]-'a']+=1;
+	    int f = 0;
+	    int x = (str.length()+1)/2;
+	    for(int i=0; i<26; i++)
+	    {
+	        if(c[i]>x)
+	            f = 1;
+	    }
+	    if(f)
+	    {
+	        if(str1=="-1")
+	            cout<<0<<endl;
+	        else
+	            cout<<1<<endl;
+	    }
+	    else
+	    {
+    	    int a[26] = {0};
+    	    int b[26] = {0};
+    	    for(int i=0; i<str.length(); i++)
+    	        a[str[i]-'a']+=1;
+    	    for(int i=0; i<str1.length(); i++)
+    	        b[str1[i]-'a']+=1;
+    	        
+    	    for(int i=0; i<26; i++)
+    	        if(a[i]!=b[i])
+    	            flag = 0;
+    	    
+    	    for(int i=0;i<str1.length();i++)
+    	    {
+    		    if(i>0)
+    		        if(str1[i-1]==str1[i])
+    			        flag=0;
+    	    }
+    	    if(flag==1)
+    		    cout<<"1\n";
+    	    else
+    	        cout<<"0\n";
+	    }
+	}
+    return 0;
+}
+// } Driver Code Ends
