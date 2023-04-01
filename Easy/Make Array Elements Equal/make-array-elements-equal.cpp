b4fa827fec779327cb58143ddc272f6742029ce8
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-04-01 13:27:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Make Array Elements Equal/make-array-elements-equal.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minOperations(int N) {
+        // Code here
+        // return (!(N%2)) ? (long long int)N/2*(N/2): (long long int)N/2 * (N/2) + 1;
+        return N%2==0?(long)N/2*(N/2):(long)N/2*(N/2+1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
