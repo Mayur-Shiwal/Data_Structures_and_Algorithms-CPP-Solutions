5b7ec2708d0f74ae85026fa62204757ece0c8c67
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-04-11 13:12:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Length/maximum-length.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+        return (a > 2*(b + c)+2 || b > 2 * (a + c) + 2 || c > 2 * (a + b) + 2) ? -1 : a + b + c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
