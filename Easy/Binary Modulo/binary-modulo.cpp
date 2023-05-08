fd7458a1eb070545d3750e369711e4374e244825
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-05-08 11:57:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Binary Modulo/binary-modulo.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        int modulo(string s,int m)
+        {
+            //code here
+            int ans = 0;
+            for(int i=0;i<s.length();++i) ans = (s[i] == '1') ? (2 * ans + 1)%m : (2 * ans)%m;
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        int m;
+        cin>>s>>m;
+        Solution a;
+        cout<<a.modulo(s,m)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
