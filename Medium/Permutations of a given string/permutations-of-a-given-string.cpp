4c4157f2891886a3c269110916a4d06c19d18c77
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-06-09 16:14:09 +0530

    Update permutations-of-a-given-string.cpp

--- Medium/Permutations of a given string/permutations-of-a-given-string.cpp
+++ Medium/Permutations of a given string/permutations-of-a-given-string.cpp
@@ -5,20 +5,40 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    private:
+        void f(int ind, string &s, vector<string> &ans, unordered_map<string, int> &mp){
+            if(ind >= s.length()){
+                if(mp.find(s) == mp.end()){
+                    ans.push_back(s);
+                    mp[s]++;
+                }
+                return;
+            }
+            for(int i=ind;i<s.length();i++){
+                swap(s[ind], s[i]);
+                f(ind+1, s, ans, mp);
+                swap(s[ind], s[i]);
+            }
+            return;
+        }
 	public:
 		vector<string>find_permutation(string S)
 		{
-		    // Code here there
-		    sort(S.begin(), S.end());
-		    vector<string> ans;
-		    ans.push_back(S);
-		    while(next_permutation(S.begin(), S.end())) ans.push_back(S);
-		    return ans;
+		  //approach 1 using STL
+		  //  sort(S.begin(), S.end());
+		  //  vector<string> ans;
+		  //  ans.push_back(S);
+		  //  while(next_permutation(S.begin(), S.end())) ans.push_back(S);
+		  //  return ans;
+		  
+		  //approach 2
+		  vector<string> ans;
+		  unordered_map<string, int> mp;
+		  f(0, S, ans, mp);
+		  sort(ans.begin(), ans.end());
+		  return ans;
 		}
 };
-
-
-
 //{ Driver Code Starts.
 int main(){
     int t;
