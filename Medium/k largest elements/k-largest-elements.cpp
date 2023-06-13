0ded83cabf9886851676bbfff53ac068a2a7674c
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-06-13 16:06:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/k largest elements/k-largest-elements.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    // code here
+	    vector<int> nums;
+	    for(int i=0;i<n;i++) nums.push_back(arr[i]);
+	    sort(nums.begin(), nums.end(), greater<int>());
+	    vector<int> ans;
+	    if(n < k) return ans;
+	    for(int i=0;i<k;i++) ans.push_back(nums[i]);
+	    return ans;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr, n, k);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
