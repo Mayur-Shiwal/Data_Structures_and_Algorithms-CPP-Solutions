00e73b9b83110ac5650a776372aae9fcf595579f
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-07-04 17:27:43 +0530

    Update find-position-of-set-bit.cpp

--- Basic/Find position of set bit/find-position-of-set-bit.cpp
+++ Basic/Find position of set bit/find-position-of-set-bit.cpp
@@ -8,19 +8,31 @@ class Solution {
     int findPosition(int N) {
         // code here
         if(N == 0) return -1;
-        string s = bitset<32>(N).to_string();
-        reverse(s.begin(), s.end());
+      
+        //approach 1
+        // string s = bitset<32>(N).to_string();
+        // reverse(s.begin(), s.end());
+        // int cnt = 0;
+        // int i;
+        // int ind = 0;
+        // for(i=0;i<s.length();i++){
+        //     if(s[i] == '1' && cnt == 0){
+        //         ind = i+1;
+        //         cnt++;
+        //     } 
+        //     else if(s[i] == '1' && cnt > 0) return -1;
+        // }
+        // return ind;
+        
+        //approach 2
         int cnt = 0;
-        int i;
         int ind = 0;
-        for(i=0;i<s.length();i++){
-            if(s[i] == '1' && cnt == 0){
-                ind = i+1;
-                cnt++;
-            } 
-            else if(s[i] == '1' && cnt > 0) return -1;
+        while(N){
+            if(N & 1) cnt++;
+            ind++;
+            N >>= 1;
         }
-        return ind;
+        return cnt==1 ? ind : -1;
     }
 };
 
