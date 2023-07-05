06ca94eb70a672511899f537f4cb52cbba56dd86
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-07-05 16:44:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/Stock buy and sell II/stock-buy-and-sell-ii.cpp
+++ Medium/Stock buy and sell II/stock-buy-and-sell-ii.cpp
@@ -30,14 +30,37 @@ public:
 // } Driver Code Ends
 
 class Solution {
+  private:
+    int f(int ind, int n, int buy, vector<vector<int>>&dp, vector<int>&prices){
+      if(ind==n){
+          return 0;
+      }
+      if(dp[ind][buy]!=-1) return dp[ind][buy];
+      if(buy==1){
+          int bought=-prices[ind]+f(ind+1,n,0,dp,prices);
+          int not_bought=f(ind+1,n,1,dp,prices);
+          return dp[ind][buy]= max(bought,not_bought);
+      }
+      else{
+          int sold=prices[ind]+f(ind+1,n,1,dp,prices);
+          int not_sold=f(ind+1,n,0,dp,prices);
+          return dp[ind][buy]= max(sold,not_sold);
+      }
+    }
   public:
     int stockBuyAndSell(int n, vector<int> &prices) {
         // code here
-        int ans = 0;
-        for(int i=0;i<n-1;i++){
-            if(prices[i] < prices[i+1]) ans += (prices[i+1] - prices[i]);
-        }
-        return ans;
+        //approach 1
+        // int ans = 0;
+        // for(int i=0;i<n-1;i++){
+        //     if(prices[i] < prices[i+1]) ans += (prices[i+1] - prices[i]);
+        // }
+        // return ans;
+        
+        //approach2
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return f(0, n, 1, dp, prices);
+        
     }
 };
 
