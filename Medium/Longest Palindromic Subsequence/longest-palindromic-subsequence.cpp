f3dbe1a0a6790c6bfb1f934565dcb0652c82f595
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-07-19 15:36:17 +0530

    Update longest-palindromic-subsequence.cpp

--- Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -10,26 +10,35 @@ using namespace std;
 
 class Solution{
   public:
-    int lcs(string s, string t){
-        int n = s.size();
-        int m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int j=0;j<=m;j++) dp[0][j] = 0;
-        for(int i=0;i<=n;i++) dp[i][0] = 0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1] == t[j-1]) dp[i][j] = 1+ dp[i-1][j-1];
-                else dp[i][j] = max((dp[i-1][j]), dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+    // int lcs(string s, string t){
+    //     int n = s.size();
+    //     int m = t.size();
+    //     vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    //     for(int j=0;j<=m;j++) dp[0][j] = 0;
+    //     for(int i=0;i<=n;i++) dp[i][0] = 0;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=m;j++){
+    //             if(s[i-1] == t[j-1]) dp[i][j] = 1+ dp[i-1][j-1];
+    //             else dp[i][j] = max((dp[i-1][j]), dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+    int f(int i, int j, string s, vector<vector<int>> &dp){
+        if(i == j) return 1;
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i] == s[j]) dp[i][j] = 2 + f(i+1, j-1, s, dp);
+        else dp[i][j] = max(f(i+1, j, s, dp), f(i, j-1, s, dp));
+        return dp[i][j];
     }
-  
     int longestPalinSubseq(string A) {
         //code here
-        string B = A;
-        reverse(B.begin(), B.end());
-        return lcs(A, B);
+        // string B = A;
+        // reverse(B.begin(), B.end());
+        // return lcs(A, B);
+        vector<vector<int>> dp(A.length(), vector<int>(A.length(), -1));
+        return f(0, A.length()-1, A, dp);
     }
 };
 
