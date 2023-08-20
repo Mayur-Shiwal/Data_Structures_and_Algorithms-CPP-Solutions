c4a702d9434f7b1bdab76c7f718af781de8ecb91
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-08-20 16:26:08 +0530

    Update number-of-occurrence.cpp

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -12,12 +12,25 @@ public:
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int i = 0, j = 0;
-	    while(i < n && arr[i] != x) i++;
-	    if(i == n) return 0;
-	    j = i;
-	    while(j < n && arr[j] == x) j++;
-	    return j - i;
+	    //1st approach
+	   // int i = 0, j = 0;
+	   // while(i < n && arr[i] != x) i++;
+	   // if(i == n) return 0;
+	   // j = i;
+	   // while(j < n && arr[j] == x) j++;
+	   // return j - i;
+	   
+	   //2nd approach
+	   //int ans = 0;
+	   //for(int i=0;i<n;i++){
+	   //    if(arr[i] == x) ans++;
+	   //}
+	   //return ans;
+	   
+	   //3rd approach
+	   unordered_map<int, int> mp;
+	   for(int i=0;i<n;i++) mp[arr[i]]++;
+	   return (mp.find(x) != mp.end()) ? mp[x] : 0;
 	}
 };
 
