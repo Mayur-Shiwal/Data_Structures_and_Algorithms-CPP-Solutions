8552b40702fb06704236c8696456893f41855a0b
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-09-04 10:58:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    
+private:
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &mat){
+        vis[row][col] = 1;
+        int n = mat.size();
+        int m = mat[0].size();
+        //now check for L,R,U,D
+        for(int delrow=-1;delrow<=1;delrow++){
+            for(int delcol=-1;delcol<=1;delcol++){
+                if(abs(delrow) == abs(delcol)) continue;
+                int nrow = row + delrow;
+                int ncol = col + delcol;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && mat[nrow][ncol] == 'O'){
+                    dfs(nrow,ncol,vis,mat);
+                }
+            }
+        }
+    }
+    
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        //traverse for the first row and first column
+        for(int j=0;j<m;j++){
+            //for first row
+            if(!vis[0][j] && mat[0][j] == 'O'){
+                dfs(0,j,vis,mat);
+            }
+            //last row
+            if(!vis[n-1][j] && mat[n-1][j] == 'O'){
+                dfs(n-1,j,vis,mat);
+            }
+        }
+        //traverse for first column and last column
+        for(int i=0;i<n;i++){
+            //first col
+            if(!vis[i][0] && mat[i][0] == 'O'){
+                dfs(i,0,vis,mat);
+            }
+            //last col
+            if(!vis[i][m-1] && mat[i][m-1] == 'O'){
+                dfs(i,m-1,vis,mat);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && mat[i][j] == 'O'){
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+        return mat;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
