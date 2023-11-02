f8d29baf897874fb19682b96ed45dcfdf4a32326
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-11-02 10:46:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Count Non-Leaf Nodes in Tree/count-non-leaf-nodes-in-tree.cpp
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+/*Structure of the node of the tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+// you are required to complete this function
+// the function should return the count of Non-Leaf Nodes
+class Solution {
+private:
+    int f(Node* root, int &leafNodes){
+        if(root == NULL) return 0;
+        if(!root->left && !root->right) leafNodes++;
+        int leftTree = f(root->left, leafNodes);
+        int rightTree = f(root->right, leafNodes);
+        int totalNodes = leftTree + rightTree + 1;
+        return totalNodes;
+    }
+public:
+    int countNonLeafNodes(Node* root) {
+        // Code here
+        int leafNodes = 0;
+        int totalNodes = 0;
+        totalNodes = f(root, leafNodes);
+        return totalNodes - leafNodes;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        cout << ob.countNonLeafNodes(root) << "\n";
+
+    }
+
+
+    return 0;
+}
+// } Driver Code Ends
