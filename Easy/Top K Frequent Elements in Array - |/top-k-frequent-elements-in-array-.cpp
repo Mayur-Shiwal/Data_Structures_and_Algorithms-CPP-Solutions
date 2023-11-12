263569ee201d972b447723c65e2b17f443a7de73
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-11-12 10:46:36 +0530

    Updated Solution - GfG to GitHub

--- Easy/Top K Frequent Elements in Array - |/top-k-frequent-elements-in-array-.cpp
+++ Easy/Top K Frequent Elements in Array - |/top-k-frequent-elements-in-array-.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    public:
+  public:
     vector<int> topK(vector<int>& nums, int k) {
         // Code here
+        unordered_map<int, int> mp;
+        for(auto it: nums) mp[it]++;
+        priority_queue<pair<int, int>> pq;
+        for(auto it: mp) pq.push({it.second, it.first});
         vector<int> ans;
-        unordered_map<int,int> mp;
-        for(auto it:nums) mp[it]++;
-        priority_queue<pair<int,int>> pq;
-        for(auto it:mp) pq.push({it.second,it.first});
         while(k--){
-            ans.push_back(pq.top().second);
-            pq.pop();
+            auto it = pq.top(); pq.pop();
+            ans.push_back(it.second);
         }
         return ans;
     }
 };
+
 
 
 //{ Driver Code Starts.
