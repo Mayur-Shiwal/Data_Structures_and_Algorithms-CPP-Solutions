53bfd6c11707bbbcdd8d79707a3be4ce62aad065
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-12-02 12:27:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Inorder Traversal and BST/inorder-traversal-and-bst.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        // code here
+        return is_sorted(arr, arr+N);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i=0;i<N;i++)cin>>arr[i];
+        Solution ob;
+        cout<<ob.isRepresentingBST(arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
