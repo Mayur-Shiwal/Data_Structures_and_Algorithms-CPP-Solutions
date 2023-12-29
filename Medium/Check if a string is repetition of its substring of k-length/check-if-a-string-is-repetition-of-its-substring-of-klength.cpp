30e5ce7733fef3b0a4e24f755ee7774e64ebd9a6
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2023-12-29 11:08:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if a string is repetition of its substring of k-length/check-if-a-string-is-repetition-of-its-substring-of-klength.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int kSubstrConcat (int n, string s, int k)
+	{
+	    // Your Code Here
+	    if(n%k != 0) return 0;
+	    unordered_map<string, int> mp;
+	    for(int i=0;i<n;i+=k) mp[s.substr(i, k)]++;
+	    return mp.size()<=2;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		int n; cin >> n;
+		string s; cin >> s;
+		int k; cin >> k;
+        Solution  ob;
+		cout << ob.kSubstrConcat (n, s, k) << endl;
+	}
+}
+// } Driver Code Ends
