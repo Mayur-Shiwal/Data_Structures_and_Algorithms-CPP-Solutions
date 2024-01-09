3d7a39d1a22952fb0e48e78234c6525dbd064000
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2024-01-09 10:25:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code hee.
+            vector<int> ans;
+            int found = -1, n = txt.length();
+            while(found < n){
+                found = txt.find(pat, found+1);
+                if(found == string::npos) break;
+                ans.push_back(found+1);
+            }
+            if(ans.size() == 0) return {-1};
+            return ans;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
