2b5b80876ee82e67f6d85a692cbbc47dfd40f286
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2024-03-15 10:59:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+
+class Solution
+{
+    public:
+    Node* reverse(Node* &head){
+        Node* prev = NULL;
+        Node* curr = head;
+        while(curr){
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    // your task is to complete this function
+    void sort(Node **head)
+    {
+         // Code here
+        Node* head1 = *head;
+        if(!head1->next) return;
+        Node* head2 = head1->next;
+        Node* curr1 = head1;
+        Node* curr2 = head2;
+        while(curr1 && curr2){
+            if(curr2) curr1->next = curr2->next;
+            curr1 = curr1->next;
+            if(curr1) curr2->next = curr1->next;
+            curr2 = curr2->next;
+        }
+        head2 = reverse(head2);
+        while(head1->next) head1 = head1->next;
+        head1->next = head2;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
