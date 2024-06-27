e05fde32bbaa39d4eae34c4b8dad21bb0a6d50f6
Mayureshwar Shiwal <111262863+Mayur-Shiwal@users.noreply.github.com>
2024-06-27 21:47:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Toeplitz matrix/toeplitz-matrix.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isToeplitz(vector<vector<int>> &mat);
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> a(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cin >> a[i][j];
+        }
+
+        bool b = isToeplitz(a);
+
+        if (b == true)
+            cout << "true";
+        else
+            cout << "false";
+
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+
+bool isToeplitz(vector<vector<int>>& mat) {
+    // code here
+    for(int i=0;i<mat.size()-1;i++){
+        for(int j=0;j<mat[i].size()-1;j++){
+            if(mat[i][j] != mat[i+1][j+1]) return false;
+        }
+    }
+    return true;
+}
